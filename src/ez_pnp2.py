e21a06044f652160966272e5bcbaf7d8c2d39688
George Stavrinos <gstavrinos@iit.demokritos.gr>
2018-11-19 16:31:44 +0200

    Update source with better service calls and more features

--- src/ez_pnp2.py
+++ src/ez_pnp2.py
@@ -28,63 +28,64 @@ def stopPlanning(req):
 # Check if the input of the scene setup service is valid
 def validSceneSetupInput(req):
     tmp = dict()
+    tmp2 = EzSceneSetupResponse()
     info = ""
-    error_codes = "0"
+    error_codes = str(tmp2.SUCCESS)
     if len(req.finger_joint_names) == 0:
         info = "Invalid service input: No finger_joint_names provided"
-        error_codes = "1"
+        error_codes = str(tmp2.NO_FINGER_JOINTS)
         return False, info, error_codes
     if req.gripper.name == "":
         info = "Invalid service input: No gripper name provided"
-        error_codes = "6"
+        error_codes = str(tmp2.NO_NAME)
         return False, info, error_codes
     if req.gripper.graspit_file == "":
         info = "Invalid service input: No graspit filename provided for the gripper"
-        error_codes = "5"
+        error_codes = str(tmp2.NO_FILENAME)
         return False, info, error_codes
     if req.pose_factor <= 0:
         info = "Invalid service input: pose_factor cannot be negative or zero"
-        error_codes = "2"
+        error_codes = str(tmp2.INVALID_POSE_FACTOR)
         return False, info, error_codes
 
     for obj in req.objects:
         if obj.name == "":
             info = "Invalid service input: No object name provided"
-            error_codes = "6"
+            error_codes = str(tmp2.NO_NAME)
             return False, info, error_codes
         if obj.name in tmp:
             info = "Invalid service input: Duplicate name: " + obj.name
-            error_codes = "4"
+            error_codes = str(tmp2.DUPLICATE_NAME)
             return False, info, error_codes
         else:
             tmp[obj.name] = 0
         if obj.graspit_file == "" and obj.moveit_file == "":
             info = "Invalid service input: No file provided for object: " + obj.name
-            error_codes = "5"
+            error_codes = str(tmp2.NO_FILENAME)
             return False, info, error_codes
         if obj.pose.header.frame_id == "":
             info = "Invalid service input: No frame_id in PoseStamped message of object: " + obj.name
-            error_codes = "3"
+            error_codes = str(tmp2.NO_FRAME_ID)
             return False, info, error_codes
 
     for obs in req.obstacles:
         if obs.name == "":
             info = "Invalid service input: No obstacle name provided"
-            error_codes = "6"
+            error_codes = str(tmp2.NO_NAME)
             return False, info, error_codes
         if obs.name in tmp:
             info = "Invalid service input: Duplicate name: " + obs.name
-            error_codes = "4"
+            error_codes = str(tmp2.DUPLICATE_NAME)
             return False, info, error_codes
         else:
             tmp[obs.name] = 0
         if obs.graspit_file == "" and obs.moveit_file == "":
             info = "Invalid service input: No file provided for obstacle: " + obs.name
-            error_codes = "5"
+            error_codes = str(tmp2.NO_FILENAME)
             return False, info, error_codes
         if obs.pose.header.frame_id == "":
             info = "Invalid service input: No frame_id in PoseStamped message of obstacle: " + obs.name
-            error_codes = "3"
+            error_codes = str(tmp2.NO_FRAME_ID)
             return False, info, error_codes
     return True, info, error_codes
 
@@ -99,28 +100,31 @@ def fixItForGraspIt(obj, pose_factor):
         p.position.z *= pose_factor
         #TODO orientation?
     else:
-        transform = TransformStamped()
-        transform.header.stamp = rospy.Time.now()
-        transform.header.frame_id = obj.header.frame_id
-        transform.child_frame_id = "ez_fix_it_for_grasp_it"
-        transform.transform.translation.x = obj.pose.pose.position.x
-        transform.transform.translation.y = obj.pose.pose.position.y
-        transform.transform.translation.z = obj.pose.pose.position.z
-        transform.transform.rotation.x = obj.pose.pose.orientation.x
-        transform.transform.rotation.y = obj.pose.pose.orientation.y
-        transform.transform.rotation.z = obj.pose.pose.orientation.z
-        transform.transform.rotation.w = obj.pose.pose.orientation.w
-        tf_listener.setTransform(transform, "fixItForGraspIt")
+        try:
+            transform = TransformStamped()
+            transform.header.stamp = rospy.Time.now()
+            transform.header.frame_id = obj.pose.header.frame_id
+            transform.child_frame_id = "ez_fix_it_for_grasp_it"
+            transform.transform.translation.x = obj.pose.pose.position.x
+            transform.transform.translation.y = obj.pose.pose.position.y
+            transform.transform.translation.z = obj.pose.pose.position.z
+            transform.transform.rotation.x = obj.pose.pose.orientation.x
+            transform.transform.rotation.y = obj.pose.pose.orientation.y
+            transform.transform.rotation.z = obj.pose.pose.orientation.z
+            transform.transform.rotation.w = obj.pose.pose.orientation.w
+            tf_listener.setTransform(transform, "fixItForGraspIt")
 
-        trans, rot = tf_listener.lookupTransform("ez_fix_it_for_grasp_it", "world", rospy.Time(0))
+            trans, rot = tf_listener.lookupTransform("ez_fix_it_for_grasp_it", "world", rospy.Time(0))
 
-        p.position.x = trans[0] * pose_factor
-        p.position.y = trans[1] * pose_factor
-        p.position.z = trans[2] * pose_factor
-        p.orientation.x = rot[0]
-        p.orientation.y = rot[1]
-        p.orientation.z = rot[2]
-        p.orientation.w = rot[3]
+            p.position.x = trans[0] * pose_factor
+            p.position.y = trans[1] * pose_factor
+            p.position.z = trans[2] * pose_factor
+            p.orientation.x = rot[0]
+            p.orientation.y = rot[1]
+            p.orientation.z = rot[2]
+            p.orientation.w = rot[3]
+        except Exception as e:
+            print e
 
     return p
 
@@ -148,14 +152,19 @@ def scene_setup(req):
                 atd.asGraspable = True
                 atd.modelName = obj.name
                 response = add_model_srv(atd)
+                if response.returnCode != response.SUCCESS:
+                    if len(res.error_codes) == 0:
+                        res.error_codes += str(response.returnCode)
+                    else:
+                        res.error_codes += str(response.returnCode) + ","
+                else:
+                    objectID = response.modelID
+                    ez_objects[obj.name] = objectID
 
-                objectID = response.modelID
-                ez_objects[obj.name] = objectID
-
-                loadm = LoadDatabaseModelRequest()
-                loadm.model_id = objectID
-                loadm.model_pose = fixItForGraspIt(obj, req.pose_factor)
-                response = load_model_srv(loadm)
+                    loadm = LoadDatabaseModelRequest()
+                    loadm.model_id = objectID
+                    loadm.model_pose = fixItForGraspIt(obj, req.pose_factor)
+                    response = load_model_srv(loadm)
             # ---------------------------
 
             # ------ Moveit scene -------
@@ -171,13 +180,19 @@ def scene_setup(req):
                 atd.asGraspable = False
                 atd.modelName = obstacle.name
                 response = add_model_srv(atd)
-                obstacleID = response.modelID
-                ez_obstacles[obstacle.name] = obstacleID
+                if response.returnCode != response.SUCCESS:
+                    if len(res.error_codes) == 0:
+                        res.error_codes += str(response.returnCode)
+                    else:
+                        res.error_codes += str(response.returnCode) + ","
+                else:
+                    obstacleID = response.modelID
+                    ez_obstacles[obstacle.name] = obstacleID
 
-                loadm = LoadDatabaseModelRequest()
-                loadm.model_id = obstacleID
-                loadm.model_pose = fixItForGraspIt(obstacle, req.pose_factor)
-                response = load_model_srv(loadm)
+                    loadm = LoadDatabaseModelRequest()
+                    loadm.model_id = obstacleID
+                    loadm.model_pose = fixItForGraspIt(obstacle, req.pose_factor)
+                    response = load_model_srv(loadm)
             # ---------------------------
 
             # ------ Moveit scene -------
@@ -193,14 +208,30 @@ def scene_setup(req):
         atd.modelName = req.gripper.name
         atd.jointNames = req.finger_joint_names
         response = add_model_srv(atd)
-        robotID = response.modelID
+        if response.returnCode != response.SUCCESS:
+            if len(res.error_codes) == 0:
+                res.error_codes += str(response.returnCode)
+            else:
+                res.error_codes += str(response.returnCode) + ","
+        else:
+            robotID = response.modelID
+
+            loadm = LoadDatabaseModelRequest()
+            loadm.model_id = robotID
+            p = Pose()
+            gripper_pos, gripper_rot = tf_listener.lookupTransform(req.gripper_frame, "world", rospy.Time(0))
+            p.position.x = gripper_pos[0] * req.pose_factor
+            p.position.y = gripper_pos[1] * req.pose_factor
+            p.position.z = gripper_pos[2] * req.pose_factor
+            # TODO orientation is not important (right?)
+            loadm.model_pose = p
         # ---------------------------
 
         return res
 
     except Exception as e:
         info = str(e)
-        error_codes = "-1"
+        error_codes = str(res.EXCEPTION)
         return False, info, error_codes
 
 def main():
