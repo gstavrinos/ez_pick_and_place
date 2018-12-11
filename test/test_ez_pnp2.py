7b1ce4e55d49205e3174a54ab7187b17bfa3c368
George Stavrinos <gstavrinos@iit.demokritos.gr>
2018-12-11 16:37:50 +0200

    Code to test planning

--- test/test_ez_pnp2.py
+++ test/test_ez_pnp2.py
@@ -4,15 +4,17 @@ import rospy
 
 import rospkg
 
-from ez_pick_and_place.srv import EzSceneSetup, EzSceneSetupRequest, EzSceneSetupResponse
+from ez_pick_and_place.srv import EzSceneSetup, EzSceneSetupRequest, EzSceneSetupResponse, EzStartPlanning, EzStartPlanningRequest, EzStartPlanningResponse
 from ez_pick_and_place.msg import EzModel
 from geometry_msgs.msg import PoseStamped
 
 def main():
     rospy.init_node("ez_graspit")
     scene_setup_srv = rospy.ServiceProxy("/ez_pnp/scene_setup", EzSceneSetup)
-    print "Waiting for the service to come up..."
+    print "Waiting for the services to come up..."
     rospy.wait_for_service("/ez_pnp/scene_setup")
+    start_planning_srv = rospy.ServiceProxy("/ez_pnp/start_planning", EzStartPlanning)
+    rospy.wait_for_service("/ez_pnp/start_planning")
     print "Done!"
 
     rospack = rospkg.RosPack()
@@ -64,6 +66,25 @@ def main():
     req.gripper_frame = "gripper_link"
 
     response = scene_setup_srv(req)
+
+    print response
+
+    target_place = PoseStamped()
+    target_place.header.frame_id = "world"
+    target_place.pose.position.x = 0.285
+    target_place.pose.position.y = -0.253
+    target_place.pose.position.z = 1.0
+
+    doit = EzStartPlanningRequest()
+    doit.graspit_target_object = "blue_pole_big"
+    doit.target_place = target_place
+    doit.arm_move_group = "arm"
+    doit.gripper_move_group = "gripper"
+    doit.secs_to_timeout = 120
+    # doit.allow_replanning = True
+    doit.reset_position = "up"
+
+    response = start_planning_srv(doit)
 
     print response
 
