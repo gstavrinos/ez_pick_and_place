360a8135955cc768833372ec018c1cca78061f91
George Stavrinos <gstavrinos@iit.demokritos.gr>
2019-02-27 18:05:51 +0200

    Fixed the first test and added a second more difficult one

--- test/test2_ez_pnp2.py
+++ test/test2_ez_pnp2.py
@@ -9,7 +9,7 @@ from ez_pick_and_place.msg import EzModel
 from geometry_msgs.msg import PoseStamped
 
 # Note:
-# In order to run this test you need the manos_graspit_config and roboskel_ros_resources packages!
+# In order to run this test you need the roboskel_ros_resources package!
 
 def main():
     rospy.init_node("ez_graspit")
@@ -24,11 +24,11 @@ def main():
 
     req = EzSceneSetupRequest()
     req.gripper.name = "schunk_pg70"
-    req.gripper.graspit_file = rospack.get_path("manos_graspit_config") + "/resources/models/robots/schunk_pg70/schunk_pg70_min_contacts.xml"
+    req.gripper.graspit_file = rospack.get_path("roboskel_ros_resources") + "/graspit/robots/schunk_pg70/schunk_pg70_min_contacts.xml"
     req.finger_joint_names = ["pg70_finger1_joint", "pg70_finger2_joint"]
 
     obstacle = EzModel()
-    obstacle.graspit_file = rospack.get_path("manos_graspit_config") + "/resources/models/obstacles/manos_table.xml"
+    obstacle.graspit_file = rospack.get_path("roboskel_ros_resources") + "/graspit/obstacles/manos_table.xml"
     obstacle.name = "table"
     obstacle.pose.header.frame_id = "world"
     obstacle.pose.pose.position.z = 0.43
@@ -36,32 +36,32 @@ def main():
     req.obstacles.append(obstacle)
 
     obj = EzModel()
-    obj.graspit_file = rospack.get_path("manos_graspit_config") + "/resources/models/objects/blue_pole_small.xml"
-    obj.moveit_file = rospack.get_path("roboskel_ros_resources") + "/3d_models/objects/blue_pole_small.stl"
-    obj.name = "blue_pole_small"
+    obj.graspit_file = rospack.get_path("roboskel_ros_resources") + "/graspit/objects/dinosaur_E.xml"
+    obj.moveit_file = rospack.get_path("roboskel_ros_resources") + "/3d_models/objects/dinosaur_E2.stl"
+    obj.name = "E"
     obj.pose.header.frame_id = "world"
-    obj.pose.pose.position.x = 0.285
+    obj.pose.pose.position.x = -0.285
     obj.pose.pose.position.y = 0.253
-    obj.pose.pose.position.z = 0.96
-    obj.pose.pose.orientation.x = 0.5
-    obj.pose.pose.orientation.y = 0.5
-    obj.pose.pose.orientation.z = 0.5
-    obj.pose.pose.orientation.w = 0.5
+    obj.pose.pose.position.z = 1.02
+    obj.pose.pose.orientation.x = 0
+    obj.pose.pose.orientation.y = 0
+    obj.pose.pose.orientation.z = 0
+    obj.pose.pose.orientation.w = 1
 
     req.objects.append(obj)
 
     obj = EzModel()
-    obj.graspit_file = rospack.get_path("manos_graspit_config") + "/resources/models/objects/blue_pole_big.xml"
-    obj.moveit_file = rospack.get_path("roboskel_ros_resources") + "/3d_models/objects/blue_pole_big.stl"
-    obj.name = "blue_pole_big"
+    obj.graspit_file = rospack.get_path("roboskel_ros_resources") + "/graspit/objects/lizard_Z.xml"
+    obj.moveit_file = rospack.get_path("roboskel_ros_resources") + "/3d_models/objects/lizard_Z2.stl"
+    obj.name = "Z"
     obj.pose.header.frame_id = "world"
     obj.pose.pose.position.x = -0.285
     obj.pose.pose.position.y = -0.253
-    obj.pose.pose.position.z = 0.96
-    obj.pose.pose.orientation.x = 0.5
-    obj.pose.pose.orientation.y = 0.5
-    obj.pose.pose.orientation.z = 0.5
-    obj.pose.pose.orientation.w = 0.5
+    obj.pose.pose.position.z = 1.035
+    obj.pose.pose.orientation.x = 0
+    obj.pose.pose.orientation.y = 0
+    obj.pose.pose.orientation.z = 0
+    obj.pose.pose.orientation.w = 1
 
     req.objects.append(obj)
 
@@ -72,19 +72,17 @@ def main():
     print response
 
     target_place = PoseStamped()
-    target_place.header.frame_id = "base_link"
+    target_place.header.frame_id = "world"
     target_place.pose.position.x = 0.285
     target_place.pose.position.y = 0.253
-    target_place.pose.position.z = 0.2
     target_place.pose.orientation.w = 1.0
 
     doit = EzStartPlanningRequest()
-    doit.graspit_target_object = "blue_pole_big"
+    doit.graspit_target_object = "Z"
     doit.target_place = target_place
     doit.arm_move_group = "arm"
     doit.gripper_move_group = "gripper"
     doit.secs_to_timeout = 480
-    # doit.allow_replanning = True
 
     response = start_planning_srv(doit)
 
